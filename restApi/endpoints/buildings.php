f4d04b4cb18aabca5570b4451f8facee5c107eb9
rovallec <56641456+rovallec@users.noreply.github.com>
2025-10-22 17:08:13 -0600

    fix

--- restApi/endpoints/buildings.php
+++ restApi/endpoints/buildings.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../db_kimco.php';
+require_once __DIR__ . '/../config/api_header.php';
 require_once __DIR__ . '/../middleware/auth.php';
 require_once __DIR__ . '/../utils/logger.php';
 
--- restApi/middleware/auth.php
+++ restApi/middleware/auth.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../config/api_header.php';
 /**
  * auth.php
  *
